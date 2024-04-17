aee70d6a5846adfaa5ee744d6b920d76f9413b46
Divyansh Prakash <divyansh.prakash@yahoo.com>
2024-04-17 22:51:37 +0530

    Functional API mirroring Zod's
    - Remove GenericFn
    - fix zero args edge case for Fn
    - Add tests for validation modes
    - Bump version
    - Update README

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ $ npm i @divs1210/zaphod
 
 ```typescript
 import z from 'zod'
-import { Fn, ValidationMode, check } from '@divs1210/zaphod'
+import { Fn, check } from '@divs1210/zaphod'
 
 // zod schema for integers
 const zInt = z.number().int()
@@ -37,12 +37,10 @@ const zOdd = zInt.refine(
 )
 
 // zaphod Fn incEven: (x: zEven) => zOdd 
-const incEven = Fn(
-    z.tuple([zEven]),   // schema of argslist: [zEven]
-    zOdd,               // schema of return value: zOdd
-    x => x + 1,         // actual implementation
-    ValidationMode.Both // Args / Ret / Both / None
-)
+const incEven = Fn()
+    .args(zEven)
+    .returns(zOdd)
+    .implement(x => x + 1)
 ```
 
 `incEven` is now a fully statically typechecked + runtime validated function!
@@ -53,45 +51,69 @@ incEven('2') // => TS error (string is not number)
 incEven(1)   // => validation error (1 is not even)
 ```
 
-## Generic functions
+### Validation Modes
 
 ```typescript
-import { GenericFn } from '@divs1210/zaphod'
+import { ValidationMode } from '@divs1210/zaphod'
 
-// X, Y, and L are called "PredVars"
-const map = GenericFn(<X extends Pred, Y extends Pred>({ X, Y, L }: { X: X, Y: Y, L: number }) =>
-    Fn(
-        z.tuple([                         // argslist: [
-            z.array(X).length(L),         //   X[] of length L
-            z.function(z.tuple([X]), Y)   //   (x: X) => Y
-        ]),                               // ]
-        z.array(Y).length(L),             // return: Y[] of length L
-        (xs, f) => xs.map(x => f(x)),
-        ValidationMode.Both
-    )
-)
+const incEven = Fn()
+    .args(zEven)
+    .returns(zOdd)
+    // disable runtime validation
+    .setValidationMode(ValidationMode.None)
+    .implement(x => x + 1)
+```
+
+The default mode is `Both`.
+
+Others available are `Args`, `Ret`, and `None`.
+
+### Generic functions
+
+```typescript
+const map = <X extends Schema, Y extends Schema>(X: X, Y: Y, L: number) => Fn()
+    .args(                                    // argslist: [
+        z.array(X).length(L),                 //   X[] of length L
+        z.function().args(X).returns(Y)       //   (x: X) => Y
+    )                                         // ]
+    .returns(z.array(Y).length(L))            // returns: Y[] of length L
+    .implement((xs, f) => xs.map(x => f(x)))
 
 const xs = [1, 2, 3]
-const ys = map({ X: zInt, Y: zInt, L: xs.length })(xs, x => x + 1)
+const ys = map(zInt, zInt, xs.length)(xs, x => x + 1)
 ```
 
-## Edge cases
+The call to `map` might look weird,
 
-### Empty argslist
+but it is roughly equivalent to the following pseudo-TS:
 
 ```typescript
-import { EmptyTuple } from '@divs1210/zaphod'
-
-const f = Fn(
-    EmptyTuple,   // empty argslist
-    z.number(),
-    () => 1, 
-    ValidationMode.Both
-)
-
-f() // TS complains that no args were provided
+const ys = map<number, number, xs.length>(xs, x => x + 1)
 ```
 
+## Use cases
+
+### ...but Zod already has `z.function()`?
+
+If you squint, you might see dependent types in here...
+
+Yes, Zod already provides a way to define function schemas and implement them:
+
+```typescript
+const f = z
+    .function()
+    .args(z.number())
+    .returns(z.string())
+    .implement(x => '' + x)
+```
+
+but it ALWAYS validates the arguments, and also the return (if provided).
+
+Zaphod allows you to turn off validation for arguments, return values, or both -
+enabling you to write code in a style akin to dependent typing.
+
+You can validate all functions during development,
+and disable validation for all but the edge functions (I/O) in production.
 
 ## License
 
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,25 +1,23 @@
 import z from 'zod'
-import { EmptyTuple, Fn, GenericFn, Pred, ValidationMode, check } from '..'
+import { Fn, Schema, ValidationMode, check } from '..'
 
 const zInt = z.number().int()
 
+const zEven = zInt.refine(
+    x => x % 2 === 0,
+    'should be even'
+)
+
+const zOdd = zInt.refine(
+    x => !check(zEven, x),
+    'should be odd'
+)
+
 describe(Fn, () => {
-    const zEven = zInt.refine(
-        x => x % 2 === 0,
-        'should be even'
-    )
-
-    const zOdd = zInt.refine(
-        x => !check(zEven, x),
-        'should be odd'
-    )
-
-    const incEven = Fn(
-        z.tuple([zEven]),
-        zOdd,
-        x => x + 1,
-        ValidationMode.Both
-    )
+    const incEven = Fn()
+        .args(zEven)
+        .returns(zOdd)
+        .implement(x => x + 1)
 
     it('incEven returns an odd number', () => {
         let x = 2
@@ -36,36 +34,79 @@ describe(Fn, () => {
         expect(() => incEven(3)).toThrow('should be even')
     })
 
-    it('throws if an invalid value is returned', () => {
-        const f = Fn(
-            EmptyTuple,
-            z.string(),
+
+    const addX = Fn()
+        .args(zInt)
+        .returns(z.function().args(zInt).returns(zInt))
+        .implement(x => y => x + y)
+
+    it('addX returns adding function', () => {
+        const add5 = addX(5)
+        expect(check(z.function().args(zInt).returns(zInt), add5)).toBe(true)
+    })
+
+    it('function returned by addX works as expected', () => {
+        const add5 = addX(5)
+        expect(add5(1)).toBe(6)
+    })
+
+
+    it('honours validation mode', () => {
+        const f1 = Fn()
+            .returns(z.string())
             // @ts-expect-error
-            () => 1,
-            ValidationMode.Ret
-        )
-        expect(f).toThrow('Expected string')
+            .implement(() => 1)
+
+        const f2 = Fn()
+            .args(zInt)
+            .returns(z.string())
+            .setValidationMode(ValidationMode.None)
+            // @ts-expect-error
+            .implement(x => 1)
+        
+        const f3 = Fn()
+            .returns(z.string())
+            .setValidationMode(ValidationMode.Args)
+            // @ts-expect-error
+            .implement(() => 1)
+
+        const f4 = Fn()
+            .args(zInt)
+            .returns(zInt)
+            .setValidationMode(ValidationMode.Ret)
+            .implement(x => 1)
+
+        expect(f1).toThrow('Expected string')
+        // @ts-expect-error
+        expect(f2('hi')).toBe(1)
+        expect(f3()).toBe(1)
+        // @ts-expect-error
+        expect(f4('hi')).toBe(1)
     })
 })
 
 
-// ## Generic fn
-// ==============
-describe(GenericFn, () => {
-    const map = GenericFn(<X extends Pred, Y extends Pred>({ X, Y, L }: { X: X, Y: Y, L: number }) =>
-        Fn(
-            z.tuple([
-                z.array(X).length(L),
-                z.function(z.tuple([X]), Y)
-            ]),
-            z.array(Y).length(L),
-            (xs, f) => xs.map(x => f(x)),
-            ValidationMode.Both
+describe('Generic Fns', () => {
+    const map = <X extends Schema, Y extends Schema>(X: X, Y: Y, L: number) => Fn()
+        .args(
+            z.array(X).length(L),
+            z.function().args(X).returns(Y)
         )
-    )
+        .returns(z.array(Y).length(L))
+        .implement((xs, f) => xs.map(x => f(x)))
+
+    const reduce = <X extends Schema, Acc extends Schema>(X: X, Acc: Acc) => Fn()
+        .args(
+            z.array(X),
+            z.function().args(Acc, X).returns(Acc),
+            Acc
+        )
+        .returns(Acc)
+        .implement((xs, f, init) => xs.reduce((acc, x) => f(acc, x), init))
 
     let xs = [1, 2, 3]
-    let ys = map({ X: zInt, Y: zInt, L: xs.length })(xs, x => x + 1)
+    let ys = map(zInt, zInt, xs.length)(xs, x => x + 1)
+    let zs = reduce(zInt, z.string())(xs, (acc, x) => acc + x, '')
 
     it('map returns Y[]', () => {
         expect(check(z.array(zInt), ys)).toBe(true)
@@ -77,5 +118,9 @@ describe(GenericFn, () => {
 
     it('map returns array of same length as input', () => {
         expect(ys.length).toBe(xs.length)
+    })
+
+    it('map returns array of same length as input', () => {
+        expect(zs).toBe('123')
     })
 })
